4a20c50aba6ed18ea5f39f0314b26114b5bac4f4
Brian Campbell <brian.campbell003@gmail.com>
2023-08-01 12:11:29 -0500

    contact page styles

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import emailjs from "@emailjs/browser";
-
+import './Contact.css';
 
 export default function Contact() {
   const emailRef = useRef();
@@ -29,35 +29,46 @@ export default function Contact() {
   };
 
   return (
-    <div>
+    <div className='contact-container'>
       <h1>→ Contact</h1>
-      <div>
-        <p>
-          Find me on LINKEDIN
+      <div className='email-me-div'>
+        <p className='email-me-text'>
+          Reach out via email using this form:
         </p>
-        <p>
-          Find me on GITHUB
-        </p>
-        <p>
-          Find me on SOUNDCLOUD
-        </p>
-        <p>
-          Check out my writing on MY OTHER SITE
-        </p>
-        <p>
-          Or reach out to my email - brian.campbell003@gmail.com - using this form:
-        </p>
+
+        <form className='email-me-form' onSubmit={handleSubmit}>
+          <div className='email-me-form-name-and-email'>
+            <div className='email-me-form-name'>
+              <input ref={nameRef} placeholder="first last" />
+              <label htmlFor=''>(your name)</label>
+            </div>
+            <div className='email-me-form-email'>
+              <input ref={emailRef} type="email" placeholder="you@internet.com" />
+              <label htmlFor=''>(your email)</label>
+            </div>
+          </div>
+          <div>
+            {/* <label htmlFor=''>Message</label> */}
+            <textarea className='email-me-form-textarea' ref={messageRef} type="text" placeholder="say something" />
+          </div>
+          <div className='email-me-form-btn'>
+            <input type="submit" value="Send" />
+          </div>
+        </form>
       </div>
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor=''>Name</label>
-        <input ref={nameRef} placeholder="your name" />
-        <label htmlFor=''>Email</label>
-        <input ref={emailRef} type="email" placeholder="your email" />
-        <label htmlFor=''>Message</label>
-        <input ref={messageRef} type="text" placeholder="say something" />
-        <input type="submit" value="Send" />
-      </form>
+      <p>
+        Find me on LINKEDIN
+      </p>
+      <p>
+        Find me on GITHUB
+      </p>
+      <p>
+        Find me on SOUNDCLOUD
+      </p>
+      <p>
+        Check out my writing on MY OTHER SITE
+      </p>
     </div>
   )
 }
