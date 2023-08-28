10e8ee01d74e2da5e098ac36cf90cb7eb9b18d7a
Brian Campbell <brian.campbell003@gmail.com>
2023-08-28 16:03:12 -0500

    trying to embded pdf resume page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import emailjs from "@emailjs/browser";
+import gitLogo from './assets/git-logo.png';
+import linkedinLogo from './assets/linkedin-logo.png';
+import wordpressLogo from './assets/wordpress-logo.png';
+import soundcloudLogo from './assets/soundcloud-logo.png';
+
 import './Contact.css';
 
 export default function Contact() {
@@ -32,7 +37,7 @@ export default function Contact() {
     <div className='contact-container'>
       <div className='email-me-div'>
         <p className='email-me-text'>
-          Reach out via email using this form:
+          Send me an email using this form:
         </p>
 
         <form className='email-me-form' onSubmit={handleSubmit}>
@@ -56,46 +61,73 @@ export default function Contact() {
         </form>
       </div>
 
-      <p>
-        Find me on LINKEDIN
-      </p>
-      <p>
-        Find me on GITHUB
-      </p>
-      <p>
-        Find me on SOUNDCLOUD
-      </p>
-      <p>
-        Check out my writing on MY OTHER SITE
-      </p>
+      <div className='other-sites'>
+        <p>
+          Find me on:
+        </p>
+        <div className='other-site-links'>
+          <a
+            className='other-site-link'
+            href='https://github.com/briancampbell003'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className='other-site-link-img'
+              src={gitLogo}
+              alt="github logo"
+              width="50"
+              heigth="50">
+            </img>
+            <p>GitHub</p>
+          </a>
+          <a
+            className='other-site-link'
+            href='https://www.linkedin.com/in/brian-campbell-978986195/'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className='other-site-link-img'
+              src={linkedinLogo}
+              alt="linkedin logo"
+              width="50"
+              heigth="50">
+            </img>
+            <p>LinkedIn</p>
+          </a>
+          <a
+            className='other-site-link'
+            href='http://briancampbelltext.com/'
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className='other-site-link-img'
+              src={wordpressLogo}
+              alt="wordpress logo"
+              width="50"
+              heigth="50">
+            </img>
+            <p>WordPress</p>
+          </a>
+          <a
+            className='other-site-link'
+            href='https://soundcloud.com/brian-campbell-32'
+            target="_blank"
+            rel="noreferrer">
+            <img
+              className='other-site-link-img'
+              src={soundcloudLogo}
+              alt="soundcloud logo"
+              width="50"
+              heigth="50">
+            </img>
+            <p>SoundCloud</p>
+          </a>
+
+        </div>
+      </div>
     </div>
   )
 }
-
-
-// export default function Contact () {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs.sendForm('service_8ejm071', 'contact_form', form.current, 'GsSqWgXBXaf-PXZD5')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//   };
-
-//   return (
-//     <form ref={form} onSubmit={sendEmail}>
-//       <label>Name</label>
-//       <input type="text" name="user_name" />
-//       <label>Email</label>
-//       <input type="email" name="user_email" />
-//       <label>Message</label>
-//       <textarea name="message" />
-//       <input type="submit" value="Send" />
-//     </form>
-//   );
-// };
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ResumePDF from './assets/Campbell_Resume2022_WebDev.pdf';
+import PDFObject from 'pdfobject';
+import ResumePDF from './assets/Campbell_Resume2022_dev.pdf';
 import downloadLogo from './assets/download-logo.png';
 
 
@@ -7,35 +8,16 @@ export default function Resume() {
   return (
     <div>
       <h1>→ Resume</h1>
-      <p>
-        <a className='download-link' href={ResumePDF} download>
-          Download resume as PDF file
-            <img
-                src={downloadLogo}
-                alt="download logo"
-                width="40"
-                heigth="40">
-            </img>
-        </a> 
-      </p>
-      <p>Education</p>
-      <ul>
-        <li>2U/BootCamp Spot Northwestern Coding Boot Camp; January 2023</li>
-        <li>Northwestern University, Evanston, Illinois: Bachelor of Arts; major: Philosophy, June 2016</li>
-        <li>GPA: 3.53 / 4.00</li>
-      </ul>
-      <p>Skills</p>
-      <ul>  
-        <li>JavaScript ES6+</li>
-        <li>CSS3</li>
-        <li>HTML5</li>
-        <li>SQL</li>
-        <li>Express</li>
-        <li>ReactJS</li>
-        <li>Node</li>
-        <li>Handlebars</li>
-        <li>Bootstrap</li>
-      </ul>
+      <a className='download-link' href={ResumePDF} download>
+        Download resume as PDF file
+        <img
+          src={downloadLogo}
+          alt="download logo"
+          width="40"
+          heigth="40">
+        </img>
+      </a>
+
     </div>
   );
 }
